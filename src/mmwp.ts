a70fac6d3daf6ba4b470fad547c153408a6e1b43
Louis-Dominique Dubeau <ldd@lddubeau.com>
2017-06-28 16:33:31 -0400

    Eliminate module code duplication.
    We just use the dashboard's main module and add just enough information to
    customize it for MMWP.

--- src/mmwp.ts
+++ src/mmwp.ts
@@ -6,7 +6,33 @@
  */
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
-import { AppModule } from "./mmwp/app.module";
+import { AppModule } from "dashboard/app.module";
+import { ChunksService } from "dashboard/chunks.service";
+import { ProcessingService } from "dashboard/processing.service";
+import { XMLFilesService } from "dashboard/xml-files.service";
+import { XMLTransformService } from "dashboard/xml-transform.service";
+
+import { ConcordanceTransformService,
+       } from "./mmwp/concordance-transform.service";
+import { CoNLLTransformService } from "./mmwp/conll-transform.service";
 
 // tslint:disable-next-line:no-floating-promises
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic([
+  ChunksService,
+  XMLFilesService,
+  ProcessingService, {
+  provide: XMLTransformService,
+  useClass: ConcordanceTransformService,
+  multi: true,
+}, {
+  provide: XMLTransformService,
+  useClass: CoNLLTransformService,
+  multi: true,
+}, {
+  provide: "Mode",
+  useValue: {
+    name: "mmwpa-mode",
+    path: "mmwp/mmwpa-mode/mmwpa-mode",
+  },
+  multi: true,
+}]).bootstrapModule(AppModule);
