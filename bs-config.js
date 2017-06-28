c7f424d55ecbc887d270bf4a38f55eb11b1eb4be
Louis-Dominique Dubeau <ldd@lddubeau.com>
2017-06-28 16:35:10 -0400

    Include a custom server configuration.

---
+++ bs-config.js
@@ -0,0 +1,9 @@
+/* global module */
+module.exports = {
+  port: 8888,
+  files: ["./build/**/*.{html,htm,css,js}"],
+  startPath: "/build/dev/lib/mmwp/index.html",
+  server: {
+    baseDir: ".",
+  },
+};
