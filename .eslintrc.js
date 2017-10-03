27c62874e11cff56c44c33bbb2d27f2317598d79
Louis-Dominique Dubeau <ldd@lddubeau.com>
2017-10-03 07:42:52 -0400

    Fix linting configuration.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,13 @@ module.exports = {
   extends: [
     "lddubeau-base"
   ],
+  env: {
+    node: true,
+  },
   rules: {
     "no-continue": "off",
     "import/no-extraneous-dependencies": "off",
     "import/no-unresolved": "off",
-    camelcase: ["off",
-                "We turn this off for now. Eventually we'll turn it on " +
-                "and make the change in one shot, API and all."],
     "no-mixed-operators": [
       "error",
       { "allowSamePrecedence": true },
---
+++ web/.eslintrc.js
@@ -0,0 +1,3 @@
+module.exports = {
+  extends: "lddubeau-base/es5"
+};
