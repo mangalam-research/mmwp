233f39fe8fe7975a7a86fddcf72d7c3f48a12dd8
Louis-Dominique Dubeau <ldd@lddubeau.com>
2018-05-29 11:07:45 -0400

    build: upgrade gulp to version 4.

--- gulptasks/gulpfile.js
+++ gulptasks/gulpfile.js
@@ -5,13 +5,13 @@ const Promise = require("bluebird");
 const path = require("path");
 const requireDir = require("require-dir");
 const replace = require("gulp-replace");
-const argparse = require("argparse");
-const config = require("./config");
-const { del, fs, newer, exec, execFileAndReport, spawn, mkdirpAsync } =
-      require("./util");
+const { ArgumentParser } = require("argparse");
+const eslintPlugin = require("gulp-eslint");
 const { execFile } = require("child-process-promise");
+const { spawn } = require("child_process");
 
-const { ArgumentParser } = argparse;
+const config = require("./config");
+const { del, fs, execFileAndReport } = require("./util");
 
 // Try to load local configuration options.
 let localConfig = {};
@@ -55,91 +55,58 @@ const options = config.options = parser.parseArgs(process.argv.slice(2));
 // this allows having code that depends on the configuration values.
 requireDir(".");
 
-function tsc(tsconfigPath, dest) {
+function runTSC(tsconfigPath, dest) {
   return execFileAndReport("./node_modules/.bin/tsc", ["-p", tsconfigPath,
                                                        "--outDir", dest]);
 }
 
-// The web part depends on the results of compiling wed.
-gulp.task("tsc", () => tsc("src/tsconfig.json", "build/dev/lib"));
+function tsc() {
+  return runTSC("src/tsconfig.json", "build/dev/lib");
+}
 
-gulp.task("copy-package-info", () => {
+function copyPackageInfo() {
   const dest = "build/";
-  return gulp.src(
-    [
-      "package.json",
-      "README.md",
-    ],
-    { base: "." })
+  return gulp.src(["package.json", "README.md"], { base: "." })
     .pipe(gulpNewer(dest))
     .pipe(gulp.dest(dest));
-});
+}
 
-gulp.task("copy-other",
-          () => gulp.src(["web/**/*.{js,html,css}",
-                          "node_modules/wed-demo/dev/lib/kitchen-sink.js",
-                          "src/**/*.{js,html,css,d.ts}"])
-          .pipe(gulp.dest("build/dev/lib/")));
+function copyOther() {
+  return gulp.src(["web/**/*.{js,html,css}",
+                   "node_modules/wed-demo/dev/lib/kitchen-sink.js",
+                   "src/**/*.{js,html,css,d.ts}"])
+    .pipe(gulp.dest("build/dev/lib/"));
+}
 
-gulp.task("copy-html",
-          () => gulp.src(["node_modules/wed-demo/dev/lib/kitchen-sink.html"])
-          .pipe(replace(
-            /(<script data-script-type="set-environment".*?>)/,
-            `$1
-process.env.DASHBOARD_BASE = "/node_modules/wed-demo/dev/lib/dashboard";
-`))
-          .pipe(gulp.dest("build/dev/lib/")));
+function runNGC() {
+  return execFileAndReport("./node_modules/.bin/ngc",
+                           ["-p", "src/tsconfig-aot.json"]);
+}
 
-gulp.task("build-dev", ["tsc", "copy-other", "copy-package-info"]);
+function createAOTMain() {
+  return gulp.src("src/mmwp.ts")
+    .pipe(replace("AppModule", "AppModuleNgFactory"))
+    .pipe(replace("app.module", "app.module.ngfactory"))
+    .pipe(replace("bootstrapModule", "bootstrapModuleFactory"))
+    .pipe(rename("mmwp-aot.ts"))
+    .pipe(gulp.dest("build/aot/"));
+}
 
-gulp.task("ngc", () =>
-          execFileAndReport("./node_modules/.bin/ngc",
-                            ["-p", "src/tsconfig-aot.json"]));
+function tscAOT() {
+  return runTSC("tsconfig-prod.json", "build/aot-interm");
+}
 
-gulp.task("create-aot-main", () =>
-          gulp.src("src/mmwp.ts")
-          .pipe(replace("AppModule", "AppModuleNgFactory"))
-          .pipe(replace("app.module", "app.module.ngfactory"))
-          .pipe(replace("bootstrapModule", "bootstrapModuleFactory"))
-          .pipe(rename("mmwp-aot.ts"))
-          .pipe(gulp.dest("build/aot/")));
+const buildAOTCompiled =
+      gulp.series(gulp.series(gulp.parallel(runNGC, createAOTMain), tscAOT),
+                  () => gulp.src(["src/**/*.{html,js,css}",
+                                  "build/aot/**/*.json",
+                                  "build/aot-interm/build/aot/**/*",
+                                  "build/aot-interm/src/**/*"])
+                  .pipe(gulp.dest("build/aot-compiled")));
 
-gulp.task("tsc-aot", ["ngc", "create-aot-main"],
-          () => tsc("tsconfig-prod.json", "build/aot-interm"));
-
-gulp.task("copy-aot", () => gulp.src("src/mmwp/internal-schemas/**/*")
-          .pipe(gulp.dest("build/aot-compiled/mmwp/internal-schemas")));
-
-gulp.task("build-aot", ["tsc-aot", "copy-aot"]);
-
-gulp.task("combine-aot", ["tsc-aot"],
-          () => gulp.src(["src/**/*.{html,js,css}",
-                          "build/aot/**/*.json",
-                          "build/aot-interm/build/aot/**/*",
-                          "build/aot-interm/src/**/*"])
-          .pipe(gulp.dest("build/aot-compiled")));
-
-gulp.task("build-aot-compiled", ["combine-aot"]);
-
-gulp.task("webpack", ["build-aot-compiled", "copy-other"], () =>
-          execFileAndReport("./node_modules/.bin/webpack", ["--color"]));
-
-gulp.task("build-prod", ["webpack"]);
-
-gulp.task("build-info", Promise.coroutine(function *task() {
-  const dest = "build/dev/lib/build-info.js";
-  const isNewer = yield newer(["lib/**", "!**/*_flymake.*"], dest);
-  if (!isNewer) {
-    return;
-  }
-
-  yield mkdirpAsync(path.dirname(dest));
-
-  yield exec("node misc/generate_build_info.js --unclean " +
-             `--module > ${dest}`);
-}));
-
-gulp.task("default", ["build-dev"]);
+function runWebpack() {
+  return execFileAndReport("./node_modules/.bin/webpack", ["--color"]);
+}
 
 //
 // Spawning a process due to this:
@@ -154,38 +121,108 @@ function runKarma(localOptions) {
   return spawn("./node_modules/.bin/karma", localOptions, { stdio: "inherit" });
 }
 
-gulp.task("test-karma", ["build-dev"],
-          () => runKarma(["start", "--single-run"]));
+function runTslint(tsconfig, tslintConfig) {
+  return spawn("./node_modules/.bin/tslint",
+               ["--project", tsconfig, "-c", tslintConfig],
+               { stdio: "inherit" });
+}
 
-gulp.task("test", ["test-karma", "tslint", "eslint"]);
+function tslintSrc() {
+  return runTslint("src/tsconfig.json", "src/tslint.json");
+}
+
+function tslintTest() {
+  return runTslint("test/tsconfig.json", "test/tslint.json");
+}
+
+const tslint = gulp.parallel(tslintSrc, tslintTest);
+
+function eslint() {
+  return gulp.src(["src/**/*.js", "*.js", "gulptasks/**/*.js", "web/**/*.js",
+                   "test/**/*.js", "!src/mmwp/internal-schemas/**/*.js"])
+    .pipe(eslintPlugin())
+    .pipe(eslintPlugin.format())
+    .pipe(eslintPlugin.failAfterError());
+}
+
+function testKarma() {
+  return runKarma(["start", "--single-run"]);
+}
+
+const test = gulp.parallel(testKarma, tslint, eslint);
 
 let packname;
-gulp.task("pack", ["test", "build-prod"],
-  () => execFile("npm", ["pack", "."], { cwd: "build" })
+function pack() {
+  return execFile("npm", ["pack", "."], { cwd: "build" })
     .then((result) => {
       packname = result.stdout.trim();
-    }));
+    });
+}
 
-gulp.task("pack-notest", ["default"],
-          () => execFile("npm", ["pack", "."], { cwd: "build" }));
+// =============
+// === tasks ===
+// =============
 
-gulp.task("install-test", ["pack"], Promise.coroutine(function *install() {
-  const testDir = "build/install_dir";
-  yield del(testDir);
-  yield fs.mkdirAsync(testDir);
-  yield fs.mkdirAsync(path.join(testDir, "node_modules"));
-  yield execFile("npm", ["install", `../${packname}`], { cwd: testDir });
-  yield del(testDir);
-}));
+// builds
 
-gulp.task("publish", ["install-test"],
-          () => execFile("npm", ["publish", packname], { cwd: "build" }));
+gulp.task("build-dev", gulp.parallel(tsc, copyOther, copyPackageInfo));
 
-gulp.task("clean", () => del(["build", "*.html"]));
+gulp.task("build-prod", gulp.series(gulp.parallel(buildAOTCompiled, copyOther),
+                                    runWebpack));
 
-gulp.task("distclean", ["clean"],
-          () => del(["downloads", "node_modules"]));
+gulp.task("default", gulp.task("build-dev"));
+
+// publishing
+
+gulp.task("pack-notest", gulp.series("build-prod", pack));
+
+gulp.task("pack", gulp.series("build-prod", gulp.parallel(test, pack)));
+
+gulp.task("install-test",
+          gulp.series("pack",
+                      Promise.coroutine(function *install() {
+                        const testDir = "build/install_dir";
+                        yield del(testDir);
+                        yield fs.mkdirAsync(testDir);
+                        yield fs.mkdirAsync(path.join(testDir, "node_modules"));
+                        yield execFile("npm", ["install", `../${packname}`],
+                                       { cwd: testDir });
+                        yield del(testDir);
+                      })));
+
+gulp.task("publish",
+          gulp.series("install-test",
+                      () => execFile("npm", ["publish", packname],
+                                     { cwd: "build" })));
+
+// linting
+
+gulp.task("tslint-src", tslintSrc);
+
+gulp.task("tslint-test", gulp.series(gulp.parallel(tsc, copyOther),
+                                     tslintTest));
+
+gulp.task("tslint", gulp.parallel("tslint-src", "tslint-test"));
+
+gulp.task("eslint", eslint);
+
+gulp.task("lint", gulp.parallel(eslint, "tslint"));
+
+// testing
+
+gulp.task("test-karma", gulp.series("build-dev", testKarma));
+
+gulp.task("test", gulp.series("build-dev", test));
+
+// watches
 
 gulp.task("watch-web", () => {
   gulp.watch(["src/**/*", "web/**/*"], ["karma"]);
 });
+
+// cleanup
+
+gulp.task("clean", () => del(["build", "*.html"]));
+
+gulp.task("distclean",
+          gulp.series("clean", () => del(["downloads", "node_modules"])));
--- gulptasks/util.js
+++ gulptasks/util.js
@@ -1,117 +1,14 @@
-const gulp = require("gulp");
-const gulpNewer = require("gulp-newer");
-const childProcess = require("child_process");
 const Promise = require("bluebird");
 const log = require("fancy-log");
 const _fs = require("fs-extra");
-const _del = require("del");
-const path = require("path");
+const del = require("del");
 const { execFile } = require("child-process-promise");
 
 const fs = Promise.promisifyAll(_fs);
 exports.fs = fs;
 
 exports.mkdirpAsync = fs.ensureDirAsync;
-exports.del = _del;
-
-const copy = exports.copy = fs.copyAsync;
-
-const cprp = exports.cprp = function cprp(src, dest) {
-  return copy(src, dest, { clobber: true, preserveTimestamps: true });
-};
-
-exports.cprpdir = function cprpdir(src, dest) {
-  if (!(src instanceof Array)) {
-    src = [src];
-  }
-  const promises = [];
-  for (const s of src) {
-    const basename = path.basename(s);
-    promises.push(cprp(s, path.join(dest, basename)));
-  }
-
-  if (promises.length === 0) {
-    return promises[0];
-  }
-
-  return Promise.each(promises, () => {});
-};
-
-exports.exec = function exec(command, options) {
-  return new Promise((resolve, reject) => {
-    childProcess.exec(command, options, (err, stdout, stderr) => {
-      if (err) {
-        log(stdout);
-        log(stderr);
-        reject(err);
-      }
-      resolve(stdout, stderr);
-    });
-  });
-};
-
-exports.checkStatusFile = function checkStatusFile(file, args, options) {
-  return new Promise((resolve) => {
-    childProcess.execFile(file, args, options,
-                          err => resolve(err ? err.code : 0));
-  });
-};
-
-exports.newer = function newer(src, dest, forceDestFile) {
-  // We use gulp-newer to perform the test and convert it to a promise.
-  const options = {
-    dest,
-  };
-
-  if (forceDestFile) {
-    options.map = function map() {
-      return ".";
-    };
-  }
-
-  return new Promise((resolve) => {
-    const stream = gulp.src(src, { read: false })
-            .pipe(gulpNewer(options));
-
-    function end() {
-      resolve(false);
-    }
-
-    stream.on("data", () => {
-      stream.removeListener("end", end);
-      stream.end();
-      resolve(true);
-    });
-
-    stream.on("end", end);
-  });
-};
-
-exports.copyIfNewer = function copyIfNewer(src, dest) {
-  return src
-    .pipe(gulpNewer(dest))
-    .pipe(gulp.dest(dest));
-};
-
-exports.spawn = function spawn(cmd, args, options) {
-  return new Promise((resolve, reject) => {
-    const child = childProcess.spawn(cmd, args || [], options || {});
-
-    child.on("exit", (code, signal) => {
-      if (code) {
-        reject(new Error(`child terminated with code: ${code}`));
-        return;
-      }
-
-      if (signal) {
-        reject(new Error(`child terminated with signal: ${signal}`));
-        return;
-      }
-
-      resolve();
-    });
-  });
-};
+exports.del = del;
 
 /**
  * Why use this over spawn with { stdio: "inherit" }? If you use this function,
