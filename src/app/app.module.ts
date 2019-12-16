6abcf44866a3c7f2644ebb774aa0e9a13a6da806
Louis-Dominique Dubeau <ldd@lddubeau.com>
2019-12-16 09:05:37 -0500

    refactor: make a static configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule } from "@angular/router";
 
-import { configuration, XMLTransformService,  XMLUploadAndTransformService,
-       } from "wed-demo-lib";
+import { ChunksService, ConfirmService, MetadataService, ModesService,
+         PacksService, ProcessingService, routes, SchemasService, SharedModule,
+         UpgradeService, UtilModule, WedDemoDashboardComponent, WedDemoModule,
+         XMLFilesService, XMLTransformService,
+         XMLUploadAndTransformService } from "wed-demo-lib";
 
 import { ConcordanceCleanupTransformService,
        } from "./concordance-cleanup-transform.service";
@@ -14,41 +20,65 @@ import { CSVTransformService } from "./csv-transform.service";
 import { SemanticInformationTransformService,
        } from "./semantic-information-transform.service";
 
-export const cf: NgModule = Object.assign({}, configuration);
-
-// tslint:disable-next-line:no-non-null-assertion
-cf.providers = cf.providers!.concat([{
-  provide: XMLTransformService,
-  useClass: ConcordanceCleanupTransformService,
-  multi: true,
-}, {
-  provide: XMLTransformService,
-  useClass: ConcordanceToDocTransformService,
-  multi: true,
-}, {
-  provide: XMLTransformService,
-  useClass: CoNLLTransformService,
-  multi: true,
-}, {
-  provide: XMLTransformService,
-  useClass: SemanticInformationTransformService,
-  multi: true,
-}, {
-  provide: XMLTransformService,
-  useClass: CSVTransformService,
-  multi: true,
-}, {
-  provide: XMLUploadAndTransformService,
-  useClass: ConcordanceUploadAndCleanupService,
-  multi: true,
-}, {
-  provide: "Mode",
-  useValue: {
-    name: "mmwpa-mode",
-    path: "../../../mmwpa-mode",
-  },
-  multi: true,
-}]);
-@NgModule(cf)
+//
+// Originally we had this module set so that it would load ``configuration``
+// from wed-demo-lib and just modify that. Turns out that AOT does not work if
+// we do that. So we have to duplicate the initialization code from wed-demo-lib
+// here. :-(
+//
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    SharedModule,
+    RouterModule.forRoot(routes, { useHash: true }),
+    UtilModule,
+    WedDemoModule,
+  ],
+  providers: [
+    ConfirmService,
+    ChunksService,
+    XMLFilesService,
+    ProcessingService,
+    ModesService,
+    SchemasService,
+    MetadataService,
+    UpgradeService,
+    PacksService,
+    // Providers above are those of wed-demo. What follows is what we add.
+    {
+      provide: XMLTransformService,
+      useClass: ConcordanceCleanupTransformService,
+      multi: true,
+    }, {
+      provide: XMLTransformService,
+      useClass: ConcordanceToDocTransformService,
+      multi: true,
+    }, {
+      provide: XMLTransformService,
+      useClass: CoNLLTransformService,
+      multi: true,
+    }, {
+      provide: XMLTransformService,
+      useClass: SemanticInformationTransformService,
+      multi: true,
+    }, {
+      provide: XMLTransformService,
+      useClass: CSVTransformService,
+      multi: true,
+    }, {
+      provide: XMLUploadAndTransformService,
+      useClass: ConcordanceUploadAndCleanupService,
+      multi: true,
+    }, {
+      provide: "Mode",
+      useValue: {
+        name: "mmwpa-mode",
+        path: "../../../mmwpa-mode",
+      },
+      multi: true,
+    }],
+  bootstrap: [ WedDemoDashboardComponent ],
+})
 // tslint:disable-next-line:no-unnecessary-class
 export class AppModule { }
